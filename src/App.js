3afdf518e39eb43a3d8ce2c31092049c43824c97
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-16 17:53:14 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
-
+import Person from "./Person/Person";
 import "./App.css";
 
-
 class App extends Component {
+
+  state = {
+    persons:  [
+      { name: 'Alfred', age: '23'},
+      { name: 'Karine', age: '11'},
+      { name: 'Masha', age: '55'},
+      { name: 'Joseph', age: '34'}
+    ]``
+  }
+
   render() {
     return (
-      <div className = "App">
+      <div className="App">
         <h1>I am React App</h1>
+ 
+        <Person name= { this.state.persons[0].name} age= { this.state.persons[0].age}  />
+        <Person name="Joseph" age="37">
+          My hobbies are : Racing and Skiing
+        </Person>
+        {/* <Person name="Joseph" age="37"> My hobbies are : Racing and Skiing </Person> */}
       </div>
-    )
+    );
 
     //this is what JSX IS
     // return React.createElement ('div', null, 'h1', "I am React App")
   }
-};
+}
 export default App;
